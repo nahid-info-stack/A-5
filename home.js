d011edc6f0bc096025e5a8cc4f4a25c751b38295
nahid-info-stack <nahidcse.inf@gmail.com>
2026-03-09 23:32:28 +0600

    added modal

--- home.js
+++ home.js
@@ -11,6 +11,72 @@ const issueLoding = () => {
 
 
 }
+const showIssue = async(id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+   // console.log(url);
+    const res = await fetch(url);
+    const show = await res.json();
+    displayIssueShow(show.data);
+};
+// {
+//     "id": 1,
+//     "title": "Fix navigation menu on mobile devices",
+//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+//     "status": "open",
+//     "labels": [
+//         "bug",
+//         "help wanted"
+//     ],
+//     "priority": "high",
+//     "author": "john_doe",
+//     "assignee": "jane_smith",
+//     "createdAt": "2024-01-15T10:30:00Z",
+//     "updatedAt": "2024-01-15T10:30:00Z"
+// }
+const displayIssueShow = (card)=> {
+    console.log(card);
+    const issueInfoContainer = document.getElementById('issue-info-container');
+    issueInfoContainer.innerHTML= `
+    <div class="bg-base-100 rounded-xl p-8 space-y-4">
+    <div>
+        <h2 class="text-2xl font-bold text-[#1F2937]">${card.title}</h2>
+        <div class="flex items-center gap-5">
+            <button class="btn btn-success">${card.status}</button>
+            <li class="text-[#64748B] text-xs">${card.assignee} Ahmed</li>
+            <li class="text-[#64748B] text-xs">22/02/2026</li>
+        </div>
+    </div>
+    <div>
+         <button class="btn btn-outline btn-error rounded-full">BUG</button>
+         <button class="btn btn-outline btn-warning rounded-full">HELP WANTED</button>
+    </div>
+    <div>
+        <p class="text-[#64748B]"> ${card.description}</p>
+    </div>
+
+    <div class="bg-gray-100 rounded flex p-4 gap-2 ">
+        <div class=" flex-1">
+            <p class="text-[#64748B]">Assignee:</p>
+            <h2 class="text-[#1F2937]">${card.assignee}</h2>
+        </div>
+        <div class="flex-1">
+            <P class="text-[#64748B]">Priority:</P>
+            <button class="btn btn-error">${card.priority}</button>
+
+        </div>
+
+    </div>
+
+</div>
+
+    
+    `;
+
+    document.getElementById('issue_modal').showModal()
+
+
+}
+
 const displayIssues = (issues) =>{
     console.log(issues);
     // 1. get the container 
@@ -27,7 +93,7 @@ const displayIssues = (issues) =>{
     const createCard = document.createElement('div');
     createCard.className = `issue-card ${issue.status === 'open' ? 'border-t-4 border-green-500' : 'border-t-4 border-purple-500'}`;
     createCard.innerHTML=`
-    <div class="card bg-base-100 rounded-lg p-4 shadow-md space-y-3  h-full">
+    <div onclick="showIssue(${issue.id})" class=" card bg-base-100 rounded-lg p-4 shadow-md space-y-3  h-full">
                 <div class="flex justify-between">
                     <div >
                     <img src="./assets/${issue.status == "open" ? "Open-Status.png" : "Closed- Status .png"}" alt="">
@@ -98,8 +164,9 @@ const activeBtn = (active)=>{
             btnName.classList.remove(...btnActive);
         
     }
-     displayIssues(filteredIssues);
  }
+     displayIssues(filteredIssues);
+
 
  for (const section of allSection) {
     section.classList.add('hidden');
